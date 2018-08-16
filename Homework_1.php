c17e826d0825604e6c7fe2c25dbda6236fb15f48
ikustow <Ak2137345>
2018-08-17 00:37:22 +0300

    php commit

--- Homework_1.php
+++ Homework_1.php
@@ -1,6 +1,29 @@
 <?php
+//Задание 1
 echo "task 1 ->\n";
+
 $name = "Илья";
 $age = "28";
 echo "\"Меня зовут: $name\"\n\"Мне $age лет\"\n";
-echo "<- End task 1";
+echo  "“!|\/’”\\";
+echo "\n<- End task 1";
+
+//Задание 2
+echo "\ntask 2 ->\n";
+
+const ALLPIC = 80;
+const FLOMPIC = 23;
+const PENCPIC = 40;
+const DRAW = ALLPIC - FLOMPIC - PENCPIC;
+
+echo DRAW;
+
+echo "\n<- End task 2";
+
+//Задание 3
+
+echo "\ntask 3 ->\n";
+
+
+
+echo "\n<- End task 3";
