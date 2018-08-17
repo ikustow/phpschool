f3a0315e90311d9af6982f71fe8026abbd18b111
ikustow <Ak2137345>
2018-08-17 23:44:56 +0300

    php commit

--- Homework1_multiplication_table.php
+++ Homework1_multiplication_table.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Admin
- * Date: 17.08.2018
- * Time: 23:13
- */
+<html>
+<body>
+<table cellpadding="5" border = "1">
+    <?php
+    for($i = 0; $i < 10; $i++) {
+        echo'<tr>';
+        for($j = 0; $j < 10; $j++)
+        {
+            $s=$j*$i;
+            echo '<td>';
+            if($i==0&&$j==0) {echo 'X';continue;}
+            if($i==0) {echo $j;continue;}
+            if($j==0) {echo $i;continue;}
+            if(($s)%2==0) print "($s)"; //здесь круглые скобки
+            else print "[$s]";
+        }
+    }
+    ?>
+</table>
+</body>
+</html>
