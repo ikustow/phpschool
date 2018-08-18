3fe7e62ba118e19b4c68c1597b25a885ec47a644
ikustow <Ak2137345>
2018-08-19 00:54:53 +0300

    php commit

--- Homework_2/src/functions.php
+++ Homework_2/src/functions.php
@@ -1,10 +1,13 @@
 <?php
 
-function task1( $string_array, $allstring = null ){
+function task1($string_array, $allstring = null){
     if ($allstring = false){
         foreach ($string_array as &$value) {
            echo "$value"."<p>";
         }
     }
-
+    if ($allstring = true){
+        $string_array = implode ($string_array);
+        return $string_array;
+    }
 }
