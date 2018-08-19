df45d08267f76ce2d2a176bbeff7f8749c9ddc2e
ikustow <Ak2137345>
2018-08-19 15:50:04 +0300

    php commit

--- Homework_1/Homework_1.php
+++ Homework_1/Homework_1.php
@@ -1,48 +1,67 @@
 <?php
 //Задание 1
+echo "<br>";
 echo "task 1 ->\n";
-
+echo "<br>";
+echo "<br>";
 $name = "Илья";
 $age = "28";
-echo "\"Меня зовут: $name\"\n\"Мне $age лет\"\n";
+echo "\"Меня зовут: $name\"";
+echo "<br>";
+echo "\"Мне $age лет\"";
+echo "<br>";
+echo "<br>";
 echo  "“!|\/’”\\";
+echo "<br>";
+echo "<br>";
 echo "\n<- End task 1";
-
+echo "<br>";
+echo "<br>";
 //Задание 2
 echo "\ntask 2 ->\n";
-
+echo "<br>";
+echo "<br>";
 const ALLPIC = 80;
 const FLOMPIC = 23;
 const PENCPIC = 40;
 const DRAW = ALLPIC - FLOMPIC - PENCPIC;
-
+echo "<br>";
 echo DRAW;
-
+echo "<br>";
+echo "<br>";
 echo "\n<- End task 2";
-
+echo "<br>";
+echo "<br>";
 //Задание 3
 
 echo "\ntask 3 ->\n";
-
+echo "<br>";
+echo "<br>";
 $age = mt_rand(1, 99);
 //echo $age;
 
 if ($age >= 18 && $age <= 65){
- echo "Вам еще работать и работать";
+    echo "<br>";
+    echo "Вам еще работать и работать";
 } elseif ($age > 65) {
+    echo "<br>";
     echo "Вам пора на пенсию";
 } elseif ($age >= 1 && $age <= 17){
+    echo "<br>";
  echo "Вам еще рано работать" ;
 } else {
+    echo "<br>";
     echo "Неизвестное число";
 }
-
+echo "<br>";
+echo "<br>";
 echo "\n<- End task 3";
-
+echo "<br>";
+echo "<br>";
 //Задание 4
 
 echo "\ntask 4 ->\n";
-
+echo "<br>";
 $day = mt_rand(1, 10);
 switch ($day) {
     case 0:
@@ -50,21 +69,26 @@ switch ($day) {
     case 2:
     case 4:
     case 5:
+    echo "<br>";
         echo "Это рабочий день";
         break;
     case 6:
     case 7:
+    echo "<br>";
         echo "Это выходной день";
         break;
     default:
+        echo "<br>";
         echo "Неизвестный день";
 }
-
+echo "<br>";
+echo "<br>";
 echo "\n <- End task 4";
-
+echo "<br>";
 //Задание 5
+echo "<br>";
 echo "\ntask 5 ->\n";
-
+echo "<br>";
 $bmw = array(
     "model" => "x5",
     "speed" => "120",
@@ -88,10 +112,14 @@ $cars["bmw"] = $bmw;
 $cars["toyota"] = $toyota;
 $cars["opel"] = $opel;
 
-
+echo "<br>";
 //print_r ($cars);
 foreach($cars  as $key => $value) {
-    echo " CAR $key \n $value[model] $value[speed] $value[doors] $value[year] \n";
+    echo "<br>";
+    echo " CAR $key ";
+    echo "<br>";
+    echo "$value[model] $value[speed] $value[doors] $value[year] ";
+    echo "<br>";
 }
-
+echo "<br>";
 echo "\n <- End task 5";
