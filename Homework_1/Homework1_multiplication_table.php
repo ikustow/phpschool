6f0426c397e35a811f7bb4937e59c956ada3c7a7
ikustow <Ak2137345>
2018-08-19 23:23:10 +0300

    php commit

--- Homework_1/Homework1_multiplication_table.php
+++ Homework_1/Homework1_multiplication_table.php
@@ -11,8 +11,11 @@
             if($i==0&&$j==0) {echo 'X';continue;}
             if($i==0) {echo $j;continue;}
             if($j==0) {echo $i;continue;}
-            if(($s)%2==0) print "($s)"; //здесь круглые скобки
-            else print "[$s]";
+            if(($i)%2==0&&($j)%2==0) {
+                print "($s)"; //здесь круглые скобки
+            }
+            elseif (($i)%2!=0&&($j)%2!=0) {print "[$s]";
+            }else print "$s";
         }
     }
     ?>
