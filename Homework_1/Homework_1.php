e5d0d4dfa242a14219acdac02cf8a0ab9bea4745
ikustow <Ak2137345>
2018-08-21 19:31:01 +0300

    php commit

--- Homework_1/Homework_1.php
+++ Homework_1/Homework_1.php
@@ -40,13 +40,13 @@ echo "<br>";
 $age = mt_rand(1, 99);
 //echo $age;
 
-if ( $age >= 18 && $age <= 65 ) {
+if ($age >= 18 && $age <= 65) {
     echo "<br>";
     echo "Вам еще работать и работать";
-} elseif ( $age > 65 ) {
+} elseif ($age > 65) {
     echo "<br>";
     echo "Вам пора на пенсию";
-} elseif ( $age >= 1 && $age <= 17 ) {
+} elseif ($age >= 1 && $age <= 17) {
     echo "<br>";
     echo "Вам еще рано работать";
 } else {
