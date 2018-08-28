8e4a6152d51d9db681719935ce8df0a509cca433
ikustow <Ak2137345>
2018-08-28 22:09:47 +0300

    php commit

--- burgers/DataBase/127_0_0_1.sql
+++ burgers/DataBase/127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 28 2018 г., 19:32
+-- Время создания: Авг 28 2018 г., 22:09
 -- Версия сервера: 5.6.38
 -- Версия PHP: 7.2.0
 
@@ -34,36 +34,56 @@ CREATE TABLE `Orders` (
   `orderID` int(11) NOT NULL,
   `clientID` int(11) DEFAULT NULL,
   `clientName` varchar(255) NOT NULL,
-  `clientEmail` varchar(255) NOT NULL
+  `clientEmail` varchar(255) NOT NULL,
+  `сomment` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `paymant` varchar(255) DEFAULT NULL,
+  `callback` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `Orders`
 --
 
-INSERT INTO `Orders` (`orderID`, `clientID`, `clientName`, `clientEmail`) VALUES
-(7, 4, 'Илья', 'ikustow@yandex.ru'),
-(8, 4, 'Илья', 'ikustow@yandex.ru'),
-(9, 4, 'Илья', 'ikustow@yandex.ru'),
-(10, 4, 'Илья', 'ikustow@yandex.ru'),
-(11, 4, 'Илья', 'ikustow@yandex.ru'),
-(12, 4, 'Илья', 'ikustow@yandex.ru'),
-(13, 4, 'Илья', 'ikustow@yandex.ru'),
-(14, 4, 'Илья', 'ikustow@yandex.ru'),
-(17, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(18, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(19, 4, 'Илья', 'ikustow@yandex.ru'),
-(20, 8, 'Кустов', 'ilya.kustov.90@mail.ru'),
-(21, 4, 'Илья', 'ikustow@yandex.ru'),
-(22, 4, 'Илья', 'ikustow@yandex.ru'),
-(23, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(24, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(25, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(26, 4, 'Илья', 'ikustow@yandex.ru'),
-(27, 7, 'Саша', 'kustova-photo@yandex.ru'),
-(28, 4, 'Илья', 'ikustow@yandex.ru'),
-(29, 4, 'Илья', 'ikustow@yandex.ru'),
-(30, 7, 'Саша', 'kustova-photo@yandex.ru');
+INSERT INTO `Orders` (`orderID`, `clientID`, `clientName`, `clientEmail`, `сomment`, `address`, `paymant`, `callback`) VALUES
+(7, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(8, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(9, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(10, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(11, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(12, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(13, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(14, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(17, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(18, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(19, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(20, 8, 'Кустов', 'ilya.kustov.90@mail.ru', NULL, NULL, NULL, NULL),
+(21, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(22, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(23, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(24, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(25, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(26, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(27, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(28, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(29, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(30, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(31, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(32, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(33, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(34, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(35, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(36, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(37, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(38, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(39, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(40, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(41, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(42, 7, 'Саша', 'kustova-photo@yandex.ru', NULL, NULL, NULL, NULL),
+(43, 4, 'Илья', 'ikustow@yandex.ru', NULL, NULL, NULL, NULL),
+(44, 4, 'Илья', 'ikustow@yandex.ru', '', 'проспект Ленина, д.25, кор.1, кв.14, эт.5', '', NULL),
+(45, 4, 'Илья', 'ikustow@yandex.ru', '1213', 'проспект Ленина, д.25, кор.1, кв.14, эт.5', 'on', NULL),
+(46, 4, 'Илья', 'ikustow@yandex.ru', '1321', 'проспект Ленина, д.25, кор.1, кв.14, эт.5', 'on', 'on');
 
 -- --------------------------------------------------------
 
@@ -85,8 +105,8 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`ID`, `client`, `email`, `orders`, `phone`, `address`) VALUES
-(4, 'Илья', 'ikustow@yandex.ru', 13, '', ', д., кор., кв., эт.'),
-(7, 'Саша', 'kustova-photo@yandex.ru', 7, '+7 (999) 999 99 99', 'проспект Ленина, д.25, кор.1, кв.14, эт.5'),
+(4, 'Илья', 'ikustow@yandex.ru', 25, '', ', д., кор., кв., эт.'),
+(7, 'Саша', 'kustova-photo@yandex.ru', 11, '+7 (999) 999 99 99', 'проспект Ленина, д.25, кор.1, кв.14, эт.5'),
 (8, 'Кустов', 'ilya.kustov.90@mail.ru', 1, '+7 (888) 888 88 88', 'проспект Ленина, д.25, кор.1, кв.14, эт.5');
 
 --
@@ -113,7 +133,7 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT для таблицы `Orders`
 --
 ALTER TABLE `Orders`
-  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT для таблицы `Users`
