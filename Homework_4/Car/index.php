8ea78b3262645b3cd454fc1504fa2edd22b31a9d
ikustow <Ak2137345>
2018-09-04 09:19:04 +0300

    php commit

--- Homework_4/Car/index.php
+++ Homework_4/Car/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once("Car.php");
-const MANUAL = "Ручная"; // Ручная коробка
-const AUTO = "Автоматическая";   // Автоматическая
-const FORWARD = "Вперед"; // едем вперед
-const BACK = "Назад"; // едем назад
+const MANUAL = "Ручная";
+const AUTO = "Автоматическая";
+const FORWARD = "Вперед";
+const BACK = "Назад";
 
 $Optima = new Kia(0, FORWARD, 500, MANUAL, 0, 12);
